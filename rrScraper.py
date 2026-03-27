9ba2552cdc0a2394f2c304194c1345e3bc3b4fea
potatoisyum <strangemudkip888@gmail.com>
2026-03-26 21:11:36 -0500

    I've changed the whole damn thing into OOP to encapsulate the data and be more efficient. Now the code should make a lot more sense.

--- rrScraper.py
+++ rrScraper.py
@@ -1,138 +1,128 @@
 """
 Royal Road doesn't do any authentication so it's possible to scrape the data directly with a request. No funny stuff needed. 
 """
-batchSize = 100 #The number of users put in one json file
+batchSize = 10 #The number of users put in one json file
 batches = 1 #number of json files generated
 
 # A list of all tags that are forcefully converted from str to int for storage in the json dumps
-convertInt = ["Follows", "Favorites", "Ratings", "Reviews", "Comments", "Fictions", "Total Words", "Total Reviews Received", "Total Ratings Received", "Followers"]
+convertInt = ["Joined", "Last Active", "Follows", "Favorites", "Ratings", "Reviews", "Comments", "Fictions", "Total Words", "Total Reviews Received", "Total Ratings Received", "Followers"]
 
 from scrapers import makeSoup
 from bs4 import BeautifulSoup
 import json
 
-# Checks the existence of a rooyal road page by looking for theh h3 404 line
-def rrExistingPage(exists):
-    # Make soup using simple soup maker
-    soup = makeSoup("https://www.royalroad.com/" + str(exists))
 
-    # Finds the big 404 letters on the 404 page
-    status = soup.find("div", class_="number font-red-sunglo")
+class scrapeRRUser(): 
+    # Initialize the library for the user in question
+    def __init__(self, userID):
+        self.user = dict({}) # dict to store all user data
+        self._userID = userID # var for userID
+        self._soupMain = makeSoup("https://www.royalroad.com/profile/" + str(userID))
+        self._soupFavorites = makeSoup("https://www.royalroad.com/profile/" + str(userID) + "/favorites")
+        self.populate()
     
-    # Checks if the 404 exists or not. If 404, returns a false for existing page. 
-    if (status == None):
-        return True
-    elif (status.text.strip() == "404"):
-        print("404 error at " + str(exists))
-        return False
-    else:
-        print("Something went wrong with the existing page detection.")
+    # Verifies an existing profile
+    def rrExistingPage(self): 
+        # Part is the section we want to search through
+        part = self._soupMain.find("div", class_="number font-red-sunglo") # The big 404 on page not found error page
+        if part == None:
+            return True
+        elif (part.text.strip() == "404"):
+            print("404 error at " + str(self._userID))
+            return False
+        else:
+            print("Something went wrong with the existing page detection.")
 
-# Scrapes main user profile given a userID assuming it exists
-def scrapeUserProfile(userID, user):
-    # Make soup using simple soup maker
-    soup = makeSoup("https://www.royalroad.com/profile/" + str(userID))
-    
-    # tbody encloses the groups of 3 on profile page tr tags enclose each line (somehow very convinient) 
-    stats = soup.find_all("tr")
-    
-    # An empty dict to store user data tied to the user's ID
-    username = soup.find("div", class_="text-center username").text.strip()
-    userData = {
-        "username" : username
-    }
-    
-    # Fill up the dict with the user data on the main profile page
-    for stats in stats:
-        # dataHead is the header to a stat and dataField is the text. Still can use stats to access the contents. 
-        dataHead = stats.find("th").text.replace(":", "").strip()
-        dataField = stats.find("td").text.strip()
+    # Method adds "Username" field to user
+    def rrScrapeUsername(self):
+        username = self._soupMain.find("div", class_="text-center username").text.strip()
+        self.user["Username"] = username # Adds to user
 
-        # Check if Field is empty and remove the entry to prevent bloating storage of empty fields
-        if (dataField == ''):
-            continue
-        # Check if Field contains a time tag to replace Field with a unix int instead of a string
-        elif (stats.find("time") != None): 
-            # Extract the int from the timeTag 
-            timeTag = stats.find("time")
-            dataField = timeTag["unixtime"]
-        
-        # Compare dataHead with convertInt for match, if match, remove commas and make an int. Print error for debugging if something weird happens
-        if (dataHead in convertInt):
+    # Scrapes main user profile and returns a list of html soup with all the data
+    def rrScrapeUserProfile(self): 
+        part = self._soupMain.find_all("tr")
+        for part in part:
+            # Finds the key and value parts of the part scraped from main soup
+            key = part.find("th").text.replace(":", "").strip()
+            value = part.find("td").text.strip()
+            if (value == ''):
+                continue
+            # Check if Field contains a time tag to replace Field with a unix int instead of a string
+            elif (part.find("time") != None): 
+                # Extract the int from the timeTag 
+                timeTag = part.find("time")
+                value = timeTag["unixtime"]
+            
+            # Compare dataHead with convertInt for match, if match, remove commas and make an int. Print error for debugging if something weird happens
+            if (key in convertInt): # TODO make convertInt a proper file and load it at the start with something
+                try:
+                    self.user[key] = int(value.replace(",", "")) # Removes , and turns it to an int
+                except: 
+                    print("Something went wrong with the forceful int change.")
+                    self.user[key] = value
+            else:
+                self.user[key] = value
+
+    # Scrapes the favorites
+    def rrScrapeUserFavorites(self): 
+        # Creates a non ordered list for fiction IDs
+        favoriteList = []
+
+        # Locates the cover image links to extract fiction IDs
+        favorites = self._soupFavorites.find_all("img", class_="cover")
+
+        for favorites in favorites:
+            ficIDLong = str(favorites["id"])
+            ficID = ficIDLong.split("-")[1]
+            
+            # Add ficID to favoriteList to be later added to user favorites. Force it to be an int. IT MUST BE INT AHHHHHHHHH
             try:
-                userData[dataHead] = int(dataField.replace(",", "")) # Removes , and turns it to an int
+                favoriteList.append(int(ficID))
             except: 
-                print("Something went wrong with the forceful int change.")
-                userData[dataHead] = dataField
-        
-    
-    user[userID] = userData
-    return user
-    print("User " + str(userID) + " added to user dict.")
+                favoriteList.append(ficID)
 
-# Scrapes user favorites given a userID given it exists
-def scrapeUserFavorites(userID, user):
-    # Make soup using simple soup maker
-    soup = makeSoup("https://www.royalroad.com/profile/" + str(userID) + "/favorites")
-    
-    # Instantiate a set that will be added under the user dict of fictions
-    favoritedFictions = []
-    
-    # Images in cover class
-    favorites = soup.find_all("img", class_="cover")
-    
-    # Seperates out the fiction id from the image 
-    for favorites in favorites:
-        ficIDLong = str(favorites["id"])
-        ficID = ficIDLong.split("-")[1]
-        
-        # Add ficID to favoritedFictions to be later added to user favorites. Force it to be an int. IT MUST BE INT AHHHHHHHHH
-        try:
-            favoritedFictions.append(int(ficID))
-        except: 
-            favoritedFictions.append(ficID)
+        # Put it into user
+        self.user ["FavoriteIDs"] = favoriteList
 
-    # Add favoritedFictions into user dict
-    user[userID].update({"FavoriteIDs" : favoritedFictions})
-    return user
+    # Initatees all the other methods to do the stuff
+    def populate(self): 
+        retrieve = self.rrExistingPage()
+        if retrieve == True:
+            self.rrScrapeUsername()
+            self.rrScrapeUserProfile()
+            self.rrScrapeUserFavorites()
+        elif retrieve == False:
+            print("UserID " + self._userID + " could not be found.")
+        else:
+            print("Existing account error")
 
+class rrBatchDump(scrapeRRUser):
+    # Initialize the dump
+    def __init__(self, size, batch):
+        self._dump = dict({})
+        self._batch = batch
+        self._size = size
+        self.populate()
 
+    # Produces the dump json
+    def getDumpJson(self): 
+        path = "Output/rr" + str(self._batch) + ".json" # Path for the dump
+        open(path, "w"). write(json.dumps(self._dump, indent=4)) # Overwrites whatever is here
+        print("Dump " + str(self._batch) + " produced in " + path)
 
-# Function that scrapes data and checks if the user exists
-def scrapeUser(userID, user):
-    # Check for valid userID
-    if (userID<=0):
-        print("Invalid userID")
+    # Scrapes the batch defined in init
+    def populate(self): 
+        # Bounds of the batch
+        start = self._size*self._batch + 1
+        end = self._size*(self._batch+1)
+        for userID in range(start, end):
+            u = scrapeRRUser(userID) # Instantiate a scrape of an RR user
+            self._dump[userID] = u.user
 
-    # Check for existing user page (not 404)
-    elif (rrExistingPage("profile/" + str(userID)) == False): 
-        print(str(userID) + " Did not have an existing user profile.")
-
-    else:
-        # Goes and scrapes the data 
-        user = scrapeUserProfile(userID, user)
-        user = scrapeUserFavorites(userID, user)
-    
-    # Return the finished user dict for this batch
-    return user
-
-# A function that scrapes users and bunches it all up in a dict to create batches in case anything is interupted 
-def batch(startUser, batchSize):
-    user = {}
-    for userID in range(startUser, startUser+batchSize-1): 
-        user = scrapeUser(userID, user)
-    # Returns the dict for it to be written to a file
-    return user
 
 if __name__ == '__main__':
     # Initializes the rr.json files, opens them up, and then puts the batches in them
     for i in range(0,batches):
-        open("Output/rr" + str(i) + ".json", "w").write("")
-        u = open("Output/rr" + str(i) + ".json", "a")
-        batchStart = 1 + i * batchSize
-        try: 
-            u.write(json.dumps(batch(batchStart, batchSize), indent=4))
-            print("Batch index " + str(i) + " complete.") # print out finished batches to represent the index of the batch
-        except: 
-            print("Batch index " + (i) + "encountered an error")
+        b = rrBatchDump(batchSize, i)
+        b.getDumpJson()
     print("Image complete.")
