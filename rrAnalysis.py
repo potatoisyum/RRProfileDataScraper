e6f27f69d63583bdba30cee9c2ec099b8861b3d1
potatoisyum <strangemudkip888@gmail.com>
2026-04-05 16:10:02 -0500

    Reads rows of database

--- rrAnalysis.py
+++ rrAnalysis.py
@@ -5,3 +5,20 @@ This file interacts with Output/royalroad.db to analyze the data in there for re
 db_path = "Output/royalroad.db"
 
 import sqlite3
+
+
+# Prints out all the contents in the database
+class readDatabase():
+    def __init__(self):
+        try:
+            with sqlite3.connect(db_path) as conn:
+                cur = conn.cursor()
+                cur.execute('SELECT Userid FROM users')
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+        except sqlite3.OperationalError as e:
+            print(e)
+
+if __name__ == '__main__':
+    readDatabase()
