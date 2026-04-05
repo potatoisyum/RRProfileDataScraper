0e8b2a760b13155ce8ad515cfa3bf247e4daff58
potatoisyum <strangemudkip888@gmail.com>
2026-04-05 16:47:26 -0500

    Properly made methods

--- rrAnalysis.py
+++ rrAnalysis.py
@@ -10,13 +10,25 @@ import sqlite3
 # Prints out all the contents in the database
 class readDatabase():
     def __init__(self):
+        self.getAllUsers()
+        
+    def getAllUsers(self):
         try:
             with sqlite3.connect(db_path) as conn:
                 cur = conn.cursor()
-                cur.execute('SELECT Userid FROM users')
+                cur.execute('SELECT Userid, Page_Exists, Username, Joined, Last_Active, Image_Time, Birthday, Gender, Location, Website, Twitter, Facebook, Bio, Follows, Ratings, Reviews, Comments, Fictions, Total_Words, Total_Reviews_Received, Total_Ratings_Received, Followers, Favorites FROM users')
                 rows = cur.fetchall()
                 for row in rows:
                     print(row)
+        except sqlite3.OperationalError as e:
+            print(e)
+    
+    # Returns the information of a user
+    def getUser(self, userid):
+        try:
+            with sqlite3.connect(db_path) as conn:
+                cur = conn.cursor()
+                cur.execute('SELECT Userid, Page_Exists, Username FROM users')
         except sqlite3.OperationalError as e:
             print(e)
 
