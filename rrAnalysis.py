440c704103146064b4672a4bf862ea765c3a20c3
potatoisyum <strangemudkip888@gmail.com>
2026-04-06 10:25:06 -0500

    Made a general user searcher and began the dict reconstructor

--- rrAnalysis.py
+++ rrAnalysis.py
@@ -15,7 +15,9 @@ import sqlite3
 class readDatabase():
     def __init__(self):
         self.getAllUsers()
-        self.getUser(2, ('Favorited',))
+        print(self.getUser(2, ['Favorited']))
+        print(self.getUserRelation(2))
+        print(self.makeDict(2))
         
     # Prints all user data
     def getAllUsers(self):
@@ -29,19 +31,41 @@ class readDatabase():
         except sqlite3.OperationalError as e:
             print(e)
 
-    # Returns the information of a user based on a tuple of requested keys
-    def getUser(self, userid, keys):
+    # Returns the information of a user based on a list of requested keys
+    def getUser(self, userid: int, keys: list):
         try:
-            search = " Userid"
+            search = keys[0]
+            keys.pop(0)
             for key in keys:
                 search += ', ' + key
             with sqlite3.connect(db_path) as conn:
                 cur = conn.cursor()
-                cur.execute('SELECT' + search + ' FROM users WHERE Userid=?', (userid,))
+                cur.execute('SELECT ' + search + ' FROM users WHERE Userid=?', (userid,))
                 rows = cur.fetchone()
-                print(rows)
+                return(rows)
         except sqlite3.OperationalError as e:
             print(e)
+    
+    # Returns the relational data in a tuple given a Userid
+    def getUserRelation(self, userid: int):
+        try:
+            with sqlite3.connect(db_path) as conn:
+                cur = conn.cursor()
+                cur.execute('SELECT Userid, Fictionid, Relation, Rating FROM relations WHERE Userid=?', (userid,))
+                rows = cur.fetchall()
+                return(rows)
+        except sqlite3.OperationalError as e:
+            print(e)
+
+    # Dict reconstructor given a userID. Returns dict
+    def makeDict(self, userid: int):
+        _attributes = ['Userid', 'Page_Exists', 'Username', 'Joined', 'Last_Active', 'Image_Time', 'Birthday', 'Gender', 'Location', 'Website', 'Twitter', 'Facebook', 'Bio', 'Follows', 'Ratings', 'Reviews', 'Comments', 'Fictions', 'Total_Words', 'Total_Reviews_Received', 'Total_Ratings_Received', 'Followers', 'Favorites', 'Favorited']
+        information = self.getUser(userid, _attributes)
+        user = dict({})
+        for i in range(0,len(_attributes)):
+            user[_attributes[i]] = information[i]
+        return user
+
 
 if __name__ == '__main__':
     readDatabase()
