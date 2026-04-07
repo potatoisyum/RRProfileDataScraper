aaae2f159481a2418e867b0a4db60c633fb87714
potatoisyum <strangemudkip888@gmail.com>
2026-04-07 16:46:45 -0500

    Tried to add relation dict reconstruction. Found out I'm generating relations wrong.

--- rrAnalysis.py
+++ rrAnalysis.py
@@ -15,6 +15,7 @@ import sqlite3
 class readDatabase():
     def __init__(self):
         self.getAllUsers()
+        self.getAllRelations()
         print(self.getUser(2, ['Favorited']))
         print(self.getUserRelation(2))
         print(self.makeDict(2))
@@ -31,9 +32,22 @@ class readDatabase():
         except sqlite3.OperationalError as e:
             print(e)
 
+    # Prints all relation data
+    def getAllRelations(self):
+        try:
+            with sqlite3.connect(db_path) as conn:
+                cur = conn.cursor()
+                cur.execute('SELECT Userid, Fictionid, Relation, Rating FROM relations')
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+        except sqlite3.OperationalError as e:
+            print(e)
+
     # Returns the information of a user based on a list of requested keys
     def getUser(self, userid: int, keys: list):
         try:
+            # Loops through the key list to format the sql
             search = keys[0]
             keys.pop(0)
             for key in keys:
@@ -50,6 +64,7 @@ class readDatabase():
     def getUserRelation(self, userid: int):
         try:
             with sqlite3.connect(db_path) as conn:
+                # Pulls the row
                 cur = conn.cursor()
                 cur.execute('SELECT Userid, Fictionid, Relation, Rating FROM relations WHERE Userid=?', (userid,))
                 rows = cur.fetchall()
@@ -62,8 +77,28 @@ class readDatabase():
         _attributes = ['Userid', 'Page_Exists', 'Username', 'Joined', 'Last_Active', 'Image_Time', 'Birthday', 'Gender', 'Location', 'Website', 'Twitter', 'Facebook', 'Bio', 'Follows', 'Ratings', 'Reviews', 'Comments', 'Fictions', 'Total_Words', 'Total_Reviews_Received', 'Total_Ratings_Received', 'Followers', 'Favorites', 'Favorited']
         information = self.getUser(userid, _attributes)
         user = dict({})
+        # Loops through everything to assign to the dict in the same order as requested from the database
         for i in range(0,len(_attributes)):
-            user[_attributes[i]] = information[i]
+            # Skip the null information to make it cleaner
+            if information[i] == None:
+                pass
+            else:
+                user[_attributes[i]] = information[i]
+        fictions = []
+        favorites = []
+        reviews = []
+        userRelations = self.getUserRelation(userid)
+        for fiction in userRelations:
+            if fiction[2] == "Fiction":
+                fictions.append(fiction[1])
+            elif fiction[2] == "Favorite":
+                favorites.append(fiction[1])
+            elif fiction[2] == "Review":
+                reviews.append([fiction[1],fiction[3]])
+        user ["FictionIDs"] = fictions
+        user ["FavoriteIDs"] = favorites
+        user ["Reviews"] = reviews
+
         return user
 
 
